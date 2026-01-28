815611075a9e3d60ed42792fe50c7563f3c198c9
James <escuderojames12@gmail.com>
2026-01-28 10:24:02 -0800

    feat: implemented items

---
+++ CakeItem.js
@@ -0,0 +1,40 @@
+class CakeItem {
+    constructor(game) {
+        this.game = game;
+        this.scale = 2;
+        this.width = 100;
+        this.height = 100;
+        
+        this.x = 1000; 
+        this.y = 950;
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./Assests/Items/Strawberrycake.png");
+
+        this.animation = new AnimatorFromOneImage(
+            this.spritesheet, 
+            0, 0,                   
+            this.width, this.height, 
+            1,                     
+            .5,                      
+            0                    
+        );
+        
+        this.animation.scale = this.scale;
+    }
+
+    update() {
+        this.y += Math.sin(this.game.timer.gameTime * 3) * 0.7;
+    }   
+
+    draw(ctx) {
+      
+        if (this.spritesheet) {
+            this.animation.drawFrame(
+                this.game.clockTick, 
+                ctx, 
+                this.x, 
+                this.y
+            );
+        }
+    }
+}
---
+++ HeartItem.js
@@ -0,0 +1,40 @@
+class HeartItem {
+    constructor(game) {
+        this.game = game;
+        this.scale = 6;
+        this.width = 19;
+        this.height = 19;
+        
+        this.x = 1200; 
+        this.y = 950;
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./Assests/Items/Hearts.png");
+
+
+        this.animation = new AnimatorFromOneImage(
+            this.spritesheet, 
+            102, 0, 
+            this.width, this.height, 
+            5,     
+            .2,    
+            5
+        );
+        
+        this.animation.scale = this.scale;
+    }
+
+    update() {
+        this.y += Math.sin(this.game.timer.gameTime * 3) * 0.7;
+    }   
+
+    draw(ctx) {
+        if (this.spritesheet) {
+            this.animation.drawFrame(
+                this.game.clockTick, 
+                ctx, 
+                this.x, 
+                this.y
+            );
+        }
+    }
+} 
