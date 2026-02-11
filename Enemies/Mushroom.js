d216ca30f1f476a29c59a64b30deb795f5167fba
James <escuderojames12@gmail.com>
2026-02-11 10:59:58 -0800

    feat: framework for Mushroom attack

--- Enemies/Mushroom.js
+++ Enemies/Mushroom.js
@@ -9,9 +9,9 @@ class Mushroom {
         this.y = 950;
 
         this.spritesheet = ASSET_MANAGER.getAsset("./Assets/Mobs/Mushroom/Mushroom-Idle.png");
+        this.spritesheetDiveAttack = ASSET_MANAGER.getAsset("./Assets/Mobs/Mushroom/Mushroom-Attack.png");
 
-
-        this.animation = new AnimatorFromOneImage(
+        this.idleAnimation = new AnimatorFromOneImage(
             this.spritesheet, 
             25, 30, 
             this.width, this.height, 
@@ -20,20 +20,91 @@ class Mushroom {
             4
         );
         
-        this.animation.scale = this.scale;
+        this.attackAnimation = new AnimatorFromOneImage(
+            this.spritesheetDiveAttack, 
+            1, 32, 
+            36, this.height, 
+            7,     
+            .15, 
+            4    
+        );
+        
+        this.idleAnimation.scale = this.scale;
+        this.attackAnimation.scale = this.scale;
 
-                this.updateBB();
+        this.updateBB();
 
+        this.state = 'idle';          
+        this.stateTimer = 0;
+        this.attackCooldown = 0;
+        this.jumpVelocity = 0;
+        this.targetX = 0;
     }
 
     update() {
-                this.updateBB();
+    this.updateBB();
 
-    }   
+    if (this.attackCooldown > 0) {
+        this.attackCooldown -= this.game.clockTick;
+    }
+
+    const player = this.game.camera?.otter;
+
+    if (player && Math.abs(player.x - this.x) < 300 && this.state === 'idle' && this.attackCooldown <= 0) {
+        this.state = 'lean-back';
+        this.stateTimer = 0;
+        this.attackAnimation.elapsedTime = 0;
+    }
+
+    switch (this.state) {
+        case 'idle':
+            break;
+
+        case 'lean-back':
+            this.stateTimer += this.game.clockTick;
+            if (this.stateTimer >= 0.5) {
+                this.state = 'jump-dive';
+                this.stateTimer = 0;
+                this.jumpVelocity = -8;
+                this.targetX = player.x;
+            }
+            break;
+
+        case 'jump-dive':
+            this.stateTimer += this.game.clockTick;
+            this.y += this.jumpVelocity;
+            this.jumpVelocity += 0.5;
+
+            const dx = this.targetX - this.x;
+            this.x += Math.sign(dx) * 4;
+
+            if (this.y >= 950) { 
+                this.y = 950;
+                this.jumpVelocity = 0;
+                this.state = 'recovery';
+                this.stateTimer = 0;
+            }
+            break;
+
+        case 'recovery':
+            this.stateTimer += this.game.clockTick;
+            if (this.stateTimer >= 0.8) {
+                this.state = 'idle';
+                this.attackCooldown = 3; 
+            }
+            break;
+    }
+}
 
     draw(ctx) {
-        if (this.spritesheet) {
-            this.animation.drawFrame(
+        let animationToUse = this.idleAnimation;
+        
+        if (this.state === 'lean-back' || this.state === 'jump-dive' || this.state === 'recovery') {
+            animationToUse = this.attackAnimation;
+        }
+
+        if (animationToUse) {
+            animationToUse.drawFrame(
                 this.game.clockTick, 
                 ctx, 
                 this.x, 
@@ -41,18 +112,25 @@ class Mushroom {
             );
         }
 
-        
         if (this.game.options.debugging) {
             ctx.strokeStyle = "Red";
             ctx.lineWidth = 5;
-            ctx.beginPath();
             ctx.strokeRect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
         }
     }
 
-
-        updateBB() {
+    updateBB() {
         this.lastBB = this.BB;
         this.BB = new BoundingBox(this.x + 35, this.y + 85, 95, 90); 
+    }
+
+
+    triggerAttackManually() {
+        if (this.state === 'idle' && this.attackCooldown <= 0) {
+            this.state = 'lean-back';
+            this.stateTimer = 0;
+            this.attackCooldown = 3;
+            console.log("Manual attack triggered!");
+        }
     }
 }
