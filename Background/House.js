caac9ea69a9b378f1a70f2d990d17eb8e6d009d3
James <escuderojames12@gmail.com>
2026-03-14 10:44:49 -0700

    bug: added hitbox for the house

--- Background/House.js
+++ Background/House.js
@@ -7,28 +7,42 @@ export class House {
         this.scale = scale;
         this.image = null;
         this.removeFromWorld = false;
+        this.BB = null; // set once image loads
 
-        // Load the image
         const img = new Image();
         img.src = imagePath;
         img.onload = () => {
             this.image = img;
+
+            const fullWidth = this.image.width * this.scale;
+            const fullHeight = this.image.height * this.scale;
+
+            // Just the door: narrow strip in the lower-center of the house
+            const doorWidth = fullWidth * 0.25;
+            const doorHeight = fullHeight * 0.5;
+
+            this.BB = new BoundingBox(
+                this.x + (fullWidth / 2) - (doorWidth / 2), // centered horizontally
+                this.y + fullHeight - doorHeight,            // bottom half
+                doorWidth,
+                doorHeight
+            );
         };
     }
 
-    update() {
-        
-    }
+    update() {}
 
     draw(ctx) {
-        if (!this.image) return;
-
-         ctx.drawImage(
-        this.image,
-        this.x,
-        this.y,
+    if (!this.image) return;
+    ctx.drawImage(this.image, this.x, this.y,
         this.image.width * this.scale,
-        this.image.height * this.scale
-    );
+        this.image.height * this.scale);
+
+    // Debug: visualize the bounding box
+    if (this.game.options.debugging && this.BB) {
+        ctx.strokeStyle = 'lime';
+        ctx.lineWidth = 3;
+        ctx.strokeRect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
     }
+}
 }
